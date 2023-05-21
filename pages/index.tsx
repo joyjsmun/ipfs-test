443698c676a0d1801fb6cca8963e5dae437d1f84
joyjsmun <joy.js.mun@gmail.com>
2023-05-21 16:23:15 +0900

    ADD: test done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,38 @@
-import { ConnectWallet, useStorageUpload } from "@thirdweb-dev/react";
+import { MediaRenderer, useStorageUpload } from "@thirdweb-dev/react";
 import { useDropzone } from "react-dropzone";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+//after upload file, display on the web
+const [uris,setUris] = useState<string[]>([]);
+console.log(uris);
+
 
   const {mutateAsync:upload} = useStorageUpload();
   const onDrop  = useCallback(
     async (acceptedFiles: File[]) => {
-      const uris =await upload({data:acceptedFiles});
-      console.log(uris);
+      const _uris =await upload({data:acceptedFiles});
+      setUris(_uris);
     },[upload],
   );
 
   const {getRootProps,getInputProps} = useDropzone({onDrop});
   return (
+    <div>
     <div {...getRootProps()}>
       <input {...getInputProps()}/>
       <button>Drop Files here to upload them to IPFS</button>
+
       
+    </div>
+    <div>
+    {uris.map(uri => {
+      return(
+        <MediaRenderer key={uri} src={uri} alt="image" />
+      )
+    })}
+    </div>
     </div>
   );
 };
